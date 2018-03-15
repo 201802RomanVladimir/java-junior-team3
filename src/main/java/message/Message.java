6659398ba7df4f1e83a8f2281ab28e56ba9c63f9
F1into <vadim.palits@gmail.com>
2018-03-15 16:58:32 +0300

    Class Message

---
+++ src/main/java/message/Message.java
@@ -0,0 +1,14 @@
+package message;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Message implements Serializable {
+
+    private String message;
+    private Date date = new Date();
+    public Message(String message) {
+        this.message = message;
+    }
+
+}
