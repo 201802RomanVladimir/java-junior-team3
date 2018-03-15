7cf377337ceca2e9c474834c44d7fe90ea42ffb2
VladimirLebedevRomanKondratiev <roman.kondratiev@complit-s.ru>
2018-03-15 17:26:50 +0300

    add acceptor

--- src/main/java/server/Acceptor.java
+++ src/main/java/server/Acceptor.java
@@ -1,4 +1,33 @@
 package server;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Acceptor {
+    public static void main(String[] args) {
+        try (ServerSocket portListener = new ServerSocket(7779)) {
+            while (true) { //Session loop
+                try (Socket clientSession = portListener.accept();
+                     InputStream inputStream = clientSession.getInputStream();
+                     ObjectInputStream in = new ObjectInputStream(inputStream)) {
+                    try {
+                        while (true) {
+                           // loggerController.log((Message) in.readObject(), new PrefixFormatVisitor());
+                        }
+                    }
+                    catch (EOFException e) {
+                        //   loggerController.flush();
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
